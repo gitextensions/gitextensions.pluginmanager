30cb11f829086aec829ad80674c28f240a522138
Ivan Grek <kvant@outlook.com>
2020-10-23 13:48:52 +0300

    Add the ability to search packages automatically on start

--- src/PackageManager.UI/App.xaml.cs
+++ src/PackageManager.UI/App.xaml.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Threading;
 using Neptuo.Activators;
@@ -105,7 +104,14 @@ namespace PackageManager
             wnd.Show();
 
             if (Args.IsSelfUpdate)
+            {
                 RunSelfUpdate(wnd);
+            }
+            else
+            {
+                wnd.ViewModel.Browser.Search
+                    .Execute();
+            }
         }
 
         private void BuildExceptionHandler()
